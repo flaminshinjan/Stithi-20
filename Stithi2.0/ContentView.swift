8df8d81cc8b9e36caa04f7139b7431d795529739
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-01 13:04:21 +0530

    Initial Commit

---
+++ Stithi2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stithi2.0
+//
+//  Created by Shinjan Patra on 01/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stithi2.0/Stithi2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Stithi2_0App.swift
+//  Stithi2.0
+//
+//  Created by Shinjan Patra on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Stithi2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
